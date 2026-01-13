1e3f983225bee242615f621799bd62cc5c90a1ae
Lingyi Zhou <61943376+reatured@users.noreply.github.com>
2026-01-13 14:55:57 -0800

    Update Vite configuration to simplify base path handling

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/remix-of-my-perspective-lifestyle/" : "/",
+export default defineConfig(() => ({
+  base: "/",
   server: {
     host: "::",
     port: 8080,
